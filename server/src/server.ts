9a9e050ba6bbabf8608762b8b7451b80d1006f15
Gintu Ciprian <ciprian.florin19g@gmail.com>
2024-09-11 01:01:15 +0300

    test: gh actions

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,6 +44,6 @@ const port = Number(process.env.SERVER_PORT) || 3000;
 const server = http.createServer(app);
 
 server.listen(port, ip, () => {
-    console.log("AA");
+    console.log("b");
     console.log(`Server is listening at http://${ip}:${port}`);
 });
